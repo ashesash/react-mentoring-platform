fb7b51b66330adf6978d42a527cba5ad6f166355
kittypsc <114567222+kittypsc@users.noreply.github.com>
2023-03-22 21:41:51 +1100

    update 22nd March 10pm

--- learnable/src/App.jsx
+++ learnable/src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Children } from "react";
+import {createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
+import Nav from './components/Nav/Navbar';
+import HomePage from './pages/HomePage';
+import WorkshopPage from './pages/WorkshopPage';
+import NewWorkshopPage from "./pages/NewWorkshop";
+
+
+const HeaderLayout = () => {
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <HeaderLayout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />
+      },
+      {
+        path: '/workshop-form',
+        element: <NewWorkshopPage />
+      },
+      {
+        path: '/workshops',
+        element: <WorkshopPage />
+      },
+
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <RouterProvider router={router}></RouterProvider>
+    
   )
 }
 
---
+++ learnable/src/pages/NewWorkshop.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import CreateWorkshopForm from '../components/CreateWorkshopForm/CreateWorkshopForm';
+
+function NewWorkshopPage() {
+    return <CreateWorkshopForm />
+
+};
+
+export default NewWorkshopPage;
