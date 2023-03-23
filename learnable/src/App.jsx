4f30b5cc51227b454b3d2939848b65c240923f37
kittypsc <114567222+kittypsc@users.noreply.github.com>
2023-03-24 10:49:03 +1100

    Revert "update 22nd March 10pm"
    This reverts commit fb7b51b66330adf6978d42a527cba5ad6f166355.

--- learnable/src/App.jsx
+++ learnable/src/App.jsx
@@ -1,48 +1,34 @@
-import React, { Children } from "react";
-import {createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
-import Nav from './components/Nav/Navbar';
-import HomePage from './pages/HomePage';
-import WorkshopPage from './pages/WorkshopPage';
-import NewWorkshopPage from "./pages/NewWorkshop";
-
-
-const HeaderLayout = () => {
-  return (
-    <div>
-      <Nav />
-      <Outlet />
-
-    </div>
-  )
-}
-
-const router = createBrowserRouter([
-  {
-    element: <HeaderLayout />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />
-      },
-      {
-        path: '/workshop-form',
-        element: <NewWorkshopPage />
-      },
-      {
-        path: '/workshops',
-        element: <WorkshopPage />
-      },
-
-    ],
-  },
-]);
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
-    <RouterProvider router={router}></RouterProvider>
-    
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
   )
 }
 
--- learnable/src/pages/NewWorkshop.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CreateWorkshopForm from '../components/CreateWorkshopForm/CreateWorkshopForm';
-
-function NewWorkshopPage() {
-    return <CreateWorkshopForm />
-
-};
-
-export default NewWorkshopPage;
