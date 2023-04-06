220dbb62fb876e7dd352408824f74599a8d201f8
AnnieL1 <annieliang17@gmail.com>
2023-04-06 11:37:59 +1000

    update workshop overview page to 5 cards

--- learnable/src/pages/WorkshopOverviewPage.jsx
+++ learnable/src/pages/WorkshopOverviewPage.jsx
@@ -20,7 +20,7 @@ useEffect(() => {
         <div className="workshops-1">
             <div className="workshops-1b">
                 <div className="workshops-2">
-                { workshopList.slice(-3).map((workshopData, key) => {
+                { workshopList.slice(-5).map((workshopData, key) => {
                 return <WorkshopCard key={key} workshopData={workshopData}/>
 
                 })}
